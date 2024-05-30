cdb7cadbb2438c1619f1c1873edff3328ef4dc8b
NihalSingh77 <nihsingh69@gmail.com>
2024-05-30 23:31:39 +0530

    Time: 15 ms (74.75%) | Memory: 11.5 MB (75.16%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,18 @@
+#40.HappyNumber
+class Solution(object):
+    def isHappy(self, n):
+        l=[]
+        while n>0:
+            temp=0
+            while n>0:
+                temp+=(n%10)**2
+                n//=10
+            if temp in l:
+                return False
+            else:
+                l.append(temp)
+            if temp==1:
+                return True
+            n=temp
+        return False
+        
